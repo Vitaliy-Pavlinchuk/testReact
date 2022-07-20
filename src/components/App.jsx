3d622de2a7c91311755039d60fd9399d69d30edf
Vitaliy-Pavlinchuk <vitaliy.pavlinchuk@gmail.com>
2022-07-20 16:24:52 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+// export const API_KEY = 'ed9b8dd4dcf22d9d746c4e21c6321e97';
+// export const API_BASE_URL = 'https://api.themoviedb.org/3';
+// export const API_IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
 import { LoginForm } from './LoginForm/LoginForm';
 import { ProductReviewForm } from './ProductReviewForm/ProductReviewForm';
 
